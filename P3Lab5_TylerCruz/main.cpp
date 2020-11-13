2c84858750433a1ad1bcfa550a8dac19816f48eb
KysTyler <62519436+KysTyler@users.noreply.github.com>
2020-11-13 16:11:01 -0600

    Security Commit to save changes

--- P3Lab5_TylerCruz/main.cpp
+++ P3Lab5_TylerCruz/main.cpp
@@ -14,17 +14,17 @@
 #include <cstdlib>
 #include <vector>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 //prototypes
-int** GenerateMatrix(int row,int col);
-int** fillMatrixManually(int**& matrix, int row,int col);
-int** fillMatrixAutomatically(int**& matrix, int row,int col);
+int** GenerateMatrix(int row, int col);
+int** fillMatrixManually(int**& matrix, int row, int col);
+int** fillMatrixAutomatically(int**& matrix, int row, int col);
 void FreeMatrix(int**& m, int row);
-void printMatrix(int**& matrix, int,int);
-int** AdditionMatrix(int**& matrixA,int**& matrixB,int size1,int size2);
-
+void printMatrix(int**& matrix, int, int);
+int** AdditionMatrix(int**& matrixA, int**& matrixB, int size1, int size2);
 
 /*
  * 
@@ -34,64 +34,75 @@ int main(int argc, char** argv) {
     vector<int**> mainArrayForMatrix;
     vector<int> vectorRow;
     vector<int> vectorCol;
-    while(mainOption != 3){
+    while (mainOption != 3) {
         cout << "*********MENU************" << endl <<
                 "Opcion #1 Ingresar matriz" << endl <<
                 "Opcion #2 Generar matriz" << endl <<
                 "Opcion #3 Realizar operación entre matrices" << endl;
         cout << "Ingrese cual opcion deseas: " << endl;
         cin >> mainOption;
-        switch(mainOption){
-            case 1:{
-                int row,col;
+        switch (mainOption) {
+            case 1:
+            {
+                int row, col;
                 cout << "Ingrese el size del row del matriz: " << endl;
                 cin >> row;
                 cout << "Ingrese el size del columna del matriz: " << endl;
                 cin >> col;
                 int** matrix = NULL;
-                matrix = GenerateMatrix(row,col);
-                matrix = fillMatrixManually(matrix, row,col);
-                
-                
+                matrix = GenerateMatrix(row, col);
+                matrix = fillMatrixManually(matrix, row, col);
+
+
                 break;
             }
-            case 2:{
-                int row,col;
+            case 2:
+            {
+                int row, col;
                 cout << "Ingrese el size del row del matriz: " << endl;
                 cin >> row;
                 cout << "Ingrese el size del columna del matriz: " << endl;
                 cin >> col;
                 int** matrix = NULL;
-                matrix = GenerateMatrix(row,col);
-                matrix = fillMatrixAutomatically(matrix, row,col);
-                
-                //
+                matrix = GenerateMatrix(row, col);
+                matrix = fillMatrixAutomatically(matrix, row, col);
+
+                //just for seeing the created matrix
                 cout << "THIS is in case 2:" << endl;
-                printMatrix(matrix,row,col);
+                printMatrix(matrix, row, col);
+
+
                 //g1.push_back(i)
                 mainArrayForMatrix.push_back(matrix);
                 vectorRow.push_back(row);
                 vectorCol.push_back(col);
                 break;
             }
-            case 3:{
+            case 3:
+            {
+                string operation = "";
                 //Print lista de matrices
-                for(int i = 0;i<mainArrayForMatrix.size();i++){
+                for (int i = 0; i < mainArrayForMatrix.size(); i++) {
                     cout << "Position of vector: " << i << endl;
                     cout << "Position of row: " << vectorRow[i] << endl;
                     cout << "Positon of col: " << vectorCol[i] << endl;
-                    printMatrix(mainArrayForMatrix.at(i),vectorRow[i],vectorCol[i]);
+                    printMatrix(mainArrayForMatrix.at(i), vectorRow[i], vectorCol[i]);
                 }
+                cout << "Ingrese la operacion que deseas realiza: " << endl;
+                cin >> operation;
+                //cout << "Operation: " << operation << endl;
+
+
                 break;
             }
-                
-                
+
+
         }
     }
     return 0;
 }
 
-int** GenerateMatrix(int row,int col) {
+int** GenerateMatrix(int row, int col) {
     if (row > 0 && col > 0) {
         int** intArray = new int*[row];
         for (int i = 0; i < row; i++) {
@@ -102,21 +113,21 @@ int** GenerateMatrix(int row,int col) {
     return NULL;
 }
 
-int** fillMatrixManually(int**& matrix, int row,int col) {
+int** fillMatrixManually(int**& matrix, int row, int col) {
     if (row > 0 && col > 0 && matrix != NULL) {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-            int x;
-            cout << "Ingrese el valor x de la posicion [" << i << "]" << "[" << j << "]" << endl;
-            cin >> x;
-            matrix[i][j] = x;
+                int x;
+                cout << "Ingrese el valor x de la posicion [" << i << "]" << "[" << j << "]" << endl;
+                cin >> x;
+                matrix[i][j] = x;
             }
         }
     }
     return matrix;
 }
 
-int** fillMatrixAutomatically(int**& matrix, int row,int col) {
+int** fillMatrixAutomatically(int**& matrix, int row, int col) {
     if (row > 0 && col > 0 && matrix != NULL) {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
@@ -140,7 +151,7 @@ void FreeMatrix(int**& m, int row) {
     }
 }
 
-void printMatrix(int**& matrix, int row,int col) {
+void printMatrix(int**& matrix, int row, int col) {
     if (row > 0 && col > 0 && matrix != NULL) {
         for (int i = 0; i < row; i++) {
             if (matrix[i] != NULL) {
@@ -153,9 +164,59 @@ void printMatrix(int**& matrix, int row,int col) {
     }
 }
 
-int** AdditionMatrix(int**& matrixA,int**& matrixB,int size1,int size2){
+int** AdditionMatrix(int**& matrixA, int**& matrixB, int size) {
     int** Tempmatrix = NULL;
-    Tempmatrix = GenerateMatrix(size1,size2);
+    Tempmatrix = GenerateMatrix(size, size);
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size<; j++) {
+            Tempmatrix[i][j] = matrixA[i][j] + matrixB[i][j];
+        }
+    }
     return Tempmatrix;
 }
+
+int** passToNegativeMatrix(int**& matrix, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            matrix[i][j] = -1 * matrix[i][j];
+        }
+    }
+    return matrix;
+}
+
+int** multOfMatrix(int**& matrixA, int**& matrixB, int rowM1, int colM1, int rowM2, int colM2) {
+    int** Tempmatrix = NULL;
+    if (colM1 == rowM2) {
+        Tempmatrix = GenerateMatrix(colM1, rowM2);
+        for(int i = 0;i < rowM1;i++){
+            for(int j = 0; j < colM2;j++){
+                int tempRowArr [rowM1] = returnArray(matrixA,i,rowM1,0);
+                int tempColArray [colM2] = returnArray(matrixB,j,colM2,1);
+                Tempmatrix[i][j] = 
+            }
+        }
+    }
+    return Tempmatrix;
+}
+
+int productoPunto(int* a, int* b){
+    
+}
+
+int* returnArray(int** m, int x, int y, int desicion) {
+    int temp [y];
+    if (desicion == 1) {
+        for (int i = 0; i < y; i++) {
+            temp[i] = m[x][i];
+        }
+    } else {
+        for (int i = 0; i < y; i++) {
+            temp[i] = m[i][x];
+        }
+    }
+    return temp;
+    
+}
+
+
 
